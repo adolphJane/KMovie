bbad373485ce314c23e818485a4dfa0c6259028c
江若铭 <adolphdeveloper@gmail.com>
2018-05-02 01:02:53 +0800

    coimmit

---
+++ Android/app/src/main/java/com/magicalrice/adolph/kmovie/business/mainhome/fragment/MainMovieFragment.java
@@ -0,0 +1,20 @@
+package com.magicalrice.adolph.kmovie.business.mainhome.fragment;
+
+import android.view.View;
+
+import com.magicalrice.adolph.kmovie.R;
+import com.magicalrice.adolph.kmovie.base.BaseFragment;
+import com.magicalrice.adolph.kmovie.databinding.FragmentMainHomeMovieBinding;
+
+public class MainMovieFragment extends BaseFragment<FragmentMainHomeMovieBinding>{
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.fragment_main_home_movie;
+    }
+
+    @Override
+    public void createView(View view) {
+
+    }
+}
